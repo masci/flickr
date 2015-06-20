22dc921407f91ad26013c0581f9f9d35ae4c0a5e
Massimiliano Pippi <mpippi@gmail.com>
2015-06-20 17:45:53 +0200

    flickr.test.login implementation

--- test/test.go
+++ test/test.go
@@ -1,9 +1,48 @@
 package test
 
 import (
+	"encoding/xml"
 	"github.com/masci/flick-rsync/flickr"
+	"io/ioutil"
 )
 
-func login(client *flickr.FlickrClient) {
-	methodName := "flickr.test.login"
+type LoginResponse struct {
+	flickr.FlickrResponse
+	User struct {
+		XMLName  xml.Name `xml:"user"`
+		ID       string   `xml:"id,attr"`
+		Username string   `xml:"username"`
+	}
+}
+
+func Login(client *flickr.FlickrClient, token *flickr.OAuthToken) (*LoginResponse, error) {
+	client.EndpointUrl = flickr.API_ENDPOINT
+
+	client.SetDefaultArgs()
+	client.Args.Set("method", "flickr.test.login")
+	client.Args.Set("oauth_token", token.OAuthToken)
+	client.Args.Set("oauth_consumer_key", client.ApiKey)
+
+	client.Sign(token.OAuthTokenSecret)
+
+	res, err := client.HTTPClient.Get(client.GetUrl())
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	loginResponse := LoginResponse{}
+	err = xml.Unmarshal([]byte(body), &loginResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO parse flickr errors
+
+	return &loginResponse, nil
 }
