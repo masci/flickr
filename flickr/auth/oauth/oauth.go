cc4e7a23453280e48167bacdb83de26b546235db
Massimiliano Pippi <mpippi@gmail.com>
2015-06-29 11:57:35 +0200

    added flickr.auth.oauth.CheckToken method

--- flickr/auth/oauth/oauth.go
+++ flickr/auth/oauth/oauth.go
@@ -1,11 +1,60 @@
 package oauth
 
-func getAuthorizeURL() {
+import (
+	"encoding/xml"
+	"github.com/masci/flickr.go/flickr"
+	flickErr "github.com/masci/flickr.go/flickr/error"
+	"io/ioutil"
+)
 
+type CheckTokenResponse struct {
+	flickr.FlickrResponse
+	OAuth struct {
+		XMLName xml.Name `xml:"oauth"`
+		Token   string   `xml:"token"`
+		Perms   string   `xml:"perms"`
+		User    struct {
+			XMLName xml.Name `xml:"user"`
+			// Flickr ID
+			ID string `xml:"nsid,attr"`
+			// Flickr Username
+			Username string `xml:"username,attr"`
+			// Flickr full name
+			Fullname string `xml:"fullname,attr"`
+		}
+	}
 }
 
-func checkToken() {
+func CheckToken(client *flickr.FlickrClient, oauthToken string) (*CheckTokenResponse, error) {
+	client.EndpointUrl = flickr.API_ENDPOINT
+	client.ClearArgs()
+	client.Args.Set("method", "flickr.auth.oauth.checkToken")
+	client.Args.Set("oauth_token", oauthToken)
+	client.Args.Set("api_key", client.ApiKey)
+	client.ApiSign(client.ApiSecret)
 
+	res, err := client.HTTPClient.Get(client.GetUrl())
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	response := CheckTokenResponse{}
+	err = xml.Unmarshal([]byte(body), &response)
+	if err != nil {
+		return nil, err
+	}
+
+	if response.HasErrors() {
+		return &response, flickErr.NewError(10)
+	}
+
+	return &response, nil
 }
 
 func getAccessToken() {
--- flickr/auth/oauth/oauth_test.go
+++ flickr/auth/oauth/oauth_test.go
@@ -1,1 +1,63 @@
 package oauth
+
+import (
+	"github.com/masci/flickr.go/flickr"
+	flickErr "github.com/masci/flickr.go/flickr/error"
+	"testing"
+)
+
+func TestCheckToken(t *testing.T) {
+	body := `<?xml version="1.0" encoding="utf-8" ?>
+	<rsp stat="ok">
+	<oauth>
+		<token>12345678901234567-12abc345def67890</token>
+		<perms>delete</perms>
+		<user nsid="12345678@N00" username="Massimiliano Pippi" fullname="Masci" />
+	</oauth>
+	</rsp>`
+
+	fclient := flickr.GetTestClient()
+	server, client := flickr.FlickrMock(200, body, "text/xml")
+	defer server.Close()
+	fclient.HTTPClient = client
+
+	resp, err := CheckToken(fclient, "12345678901234567-12abc345def67890")
+
+	if err != nil {
+		t.Error("Unexpected error", err)
+	}
+
+	flickr.Expect(t, resp.OAuth.Token, "12345678901234567-12abc345def67890")
+	flickr.Expect(t, resp.OAuth.Perms, "delete")
+	flickr.Expect(t, resp.OAuth.User.ID, "12345678@N00")
+	flickr.Expect(t, resp.OAuth.User.Username, "Massimiliano Pippi")
+	flickr.Expect(t, resp.OAuth.User.Fullname, "Masci")
+}
+
+func TestCheckTokenKo(t *testing.T) {
+	body := `<?xml version="1.0" encoding="utf-8" ?>
+	<rsp stat="fail">
+	  <err code="98" msg="Invalid token" />
+	</rsp>`
+
+	fclient := flickr.GetTestClient()
+	server, client := flickr.FlickrMock(200, body, "text/xml")
+	defer server.Close()
+	fclient.HTTPClient = client
+
+	resp, err := CheckToken(fclient, "12345678901234567-12abc345def67890")
+
+	if err == nil {
+		t.Error("Unexpected nil error")
+		t.FailNow()
+	}
+
+	ee, ok := err.(*flickErr.Error)
+	if !ok {
+		t.Error("err is not a flickErr.Error!")
+	}
+
+	flickr.Expect(t, ee.ErrorCode, 10)
+	flickr.Expect(t, resp.HasErrors(), true)
+	flickr.Expect(t, resp.ErrorCode(), 98)
+}
