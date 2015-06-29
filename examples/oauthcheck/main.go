e5ae640295d88c5f9e61adf445db1ce552b03b0d
Massimiliano Pippi <mpippi@gmail.com>
2015-06-29 11:56:42 +0200

    added example to check oauth tokens

---
+++ examples/oauthcheck/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"github.com/masci/flickr.go/flickr"
+	"github.com/masci/flickr.go/flickr/auth/oauth"
+	"os"
+)
+
+func main() {
+	// retrieve Flickr credentials from env vars
+	apik := os.Getenv("FLICKRGO_API_KEY")
+	apisec := os.Getenv("FLICKRGO_API_SECRET")
+	token := os.Getenv("FLICKRGO_OAUTH_TOKEN")
+
+	// do not proceed if credentials were not provided
+	if apik == "" || apisec == "" || token == "" {
+		fmt.Fprintln(os.Stderr, "Please set FLICKRGO_API_KEY, FLICKRGO_API_SECRET "+
+			"and FLICKRGO_OAUTH_TOKEN env vars")
+		os.Exit(1)
+	}
+
+	// create an API client with credentials
+	client := flickr.NewFlickrClient(apik, apisec)
+
+	response, _ := oauth.CheckToken(client, token)
+	fmt.Println(fmt.Sprintf("%+v", *response))
+}
