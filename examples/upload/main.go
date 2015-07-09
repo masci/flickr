8516227a3ed7d4f4489a2e038c403ab093ffd2d2
Massimiliano Pippi <mpippi@gmail.com>
2015-07-09 18:45:50 +0200

    upload example

---
+++ examples/upload/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/masci/flickr.go/flickr"
+)
+
+func main() {
+	// retrieve Flickr credentials from env vars
+	apik := os.Getenv("FLICKRGO_API_KEY")
+	apisec := os.Getenv("FLICKRGO_API_SECRET")
+	token := os.Getenv("FLICKRGO_OAUTH_TOKEN")
+	tokenSecret := os.Getenv("FLICKRGO_OAUTH_TOKEN_SECRET")
+
+	// do not proceed if credentials were not provided
+	if apik == "" || apisec == "" || token == "" || tokenSecret == "" {
+		fmt.Fprintln(os.Stderr, "Please set FLICKRGO_API_KEY, FLICKRGO_API_SECRET "+
+			"and FLICKRGO_OAUTH_TOKEN env vars")
+		os.Exit(1)
+	}
+
+	// create an API client with credentials
+	client := flickr.NewFlickrClient(apik, apisec)
+	client.OAuthToken = token
+	client.OAuthTokenSecret = tokenSecret
+
+	path, _ := filepath.Abs("examples/upload/gopher.jpg")
+	id, err := flickr.UploadPhoto(client, path, nil)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Photo uploaded, id:", id)
+	}
+}
