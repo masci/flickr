9d28b57ee864fbca4544f43ecdd2f039f85b1f35
Massimiliano Pippi <mpippi@gmail.com>
2015-07-11 12:10:49 +0200

    more tests

--- flickr/upload_test.go
+++ flickr/upload_test.go
@@ -31,4 +31,26 @@ func TestFillArgsWithParams(t *testing.T) {
 	Expect(t, client.Args.Get("content_type"), "1")
 	Expect(t, client.Args.Get("hidden"), "2")
 	Expect(t, client.Args.Get("safety_level"), "1")
+
+	params.Title = "foo"
+	params.Description = "a long description"
+	params.Tags = []string{"a", "b", "c"}
+	params.IsPublic = true
+	params.IsFamily = true
+	params.IsFriend = true
+	params.ContentType = 100
+	params.Hidden = 100
+	params.SafetyLevel = 100
+	client.ClearArgs()
+	fillArgsWithParams(client, params)
+	Expect(t, client.Args.Get("title"), "foo")
+	Expect(t, client.Args.Get("description"), "a long description")
+	Expect(t, client.Args.Get("tags"), "a b c")
+	Expect(t, client.Args.Get("is_public"), "1")
+	Expect(t, client.Args.Get("is_friend"), "1")
+	Expect(t, client.Args.Get("is_family"), "1")
+	Expect(t, client.Args.Get("content_type"), "")
+	Expect(t, client.Args.Get("hidden"), "")
+	Expect(t, client.Args.Get("safety_level"), "")
+
 }
